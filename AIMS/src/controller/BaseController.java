22f406e9ef587976cf9888d5581123af6e457665
Pham Viet Hoang <105777915+phamhoanggg@users.noreply.github.com>
2023-12-31 08:59:42 +0700

    refactor project

---
+++ AIMS/src/controller/BaseController.java
@@ -0,0 +1,37 @@
+package controller;
+
+import java.util.List;
+
+import entity.cart.Cart;
+import entity.cart.CartMedia;
+import entity.media.Media;
+
+/**
+ * This class is the base controller for our AIMS project
+ * @author nguyenlm
+ */
+// funcitonal cohesion
+// communicational cohesion
+public class BaseController {
+    
+    /**
+     * The method checks whether the Media in Cart, if it were in, we will return the CartMedia else return null
+     * @param media
+     * @return CartMedia or null
+     */
+    // datacoupling with Cart
+    // use Cart.getCart().checkMediaInCart ...
+    public CartMedia checkMediaInCart(Media media){
+        return Cart.getCart().checkMediaInCart(media);
+    }
+
+    /**
+     * This method gets the list of items in cart
+     * @return List[CartMedia]
+     */
+    //datacoupling with cart
+    // use Cart.getCart().getListMedia();
+    public List getListCartMedia(){
+        return Cart.getCart().getListMedia();
+    }
+}
---
+++ AIMS/src/entity/cart/Cart.java
@@ -0,0 +1,85 @@
+package entity.cart;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import common.exception.MediaNotAvailableException;
+import entity.media.Media;
+
+// datacoupling with CartMedia
+// stampcoupling with Media
+// functional cohesion 
+
+// Single Responsibility Principle
+public class Cart {
+    
+    private List<CartMedia> lstCartMedia;
+    private static Cart cartInstance;
+
+    // use new Cart func 
+    public static Cart getCart(){
+        if(cartInstance == null) cartInstance = new Cart();
+        return cartInstance;
+    }
+
+    private Cart(){
+        lstCartMedia = new ArrayList<>();
+    }
+
+    public void addCartMedia(CartMedia cm){
+        lstCartMedia.add(cm);
+    }
+
+    public void removeCartMedia(CartMedia cm){
+        lstCartMedia.remove(cm);
+    }
+
+    public List getListMedia(){
+        return lstCartMedia;
+    }
+
+    public void emptyCart(){
+        lstCartMedia.clear();
+    }
+  // use getQuantity() in class CartMedia
+  // Logical Cohesion
+    public int getTotalMedia(){
+        int total = 0;
+        for (Object obj : lstCartMedia) {
+            CartMedia cm = (CartMedia) obj;
+            total += cm.getQuantity();
+        }
+        return total;
+    }
+  // use getQuantity() in class CartMedia
+  // Logical Cohesion
+    public int calSubtotal(){
+        int total = 0;
+        for (Object obj : lstCartMedia) {
+            CartMedia cm = (CartMedia) obj;
+            total += cm.getPrice()*cm.getQuantity();
+        }
+        return total;
+    }
+
+    public void checkAvailabilityOfProduct() throws SQLException{
+        boolean allAvai = true;
+        for (Object object : lstCartMedia) {
+            CartMedia cartMedia = (CartMedia) object;
+            int requiredQuantity = cartMedia.getQuantity();
+            int availQuantity = cartMedia.getMedia().getQuantity();
+            if (requiredQuantity > availQuantity) allAvai = false;
+        }
+        if (!allAvai) throw new MediaNotAvailableException("Some media not available");
+    }
+
+    // only media.getId is used ? 
+    public CartMedia checkMediaInCart(Media media){
+        for (CartMedia cartMedia : lstCartMedia) {
+            if (cartMedia.getMedia().getId() == media.getId()) return cartMedia;
+        }
+        return null;
+    }
+
+}
